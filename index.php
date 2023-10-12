c31e1b5d195cc37f7fc2e6455ad0392d6fb06acf
Sachin Gavali <sachingavali321@gmail.com>
2023-10-12 05:50:46 +0530

    inputs in same file

--- index.php
+++ index.php
@@ -3,33 +3,25 @@
 <head>
    <meta charset="UTF-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>PHP</title>
-   <style>
-      li{
-         color: blue;
-         list-style: none;
-      }
-   </style>
+   <title>Form</title>
 </head>
 <body>
-    <?php
-    /*  adding simple tags 
-    echo "Hello <br>";
-    echo " <h1> Im  Sachin </h1>  "; */
-   ?> 
-   <?php
-   $name = array("Sachin","Nayan","mayur","karan","Su","off");
-    ?>
-   <h1> Names </h1>
-   <ul>
-      <?php foreach($name as $value){  ?>
-         <li> <?php 
-            if(strlen($value) >= 5){
-               echo $value;
-            }
-            ?> </li>
-         <?php } ?>
-   </ul>
-   
+
+
+<form action="" method="post">
+  Example value: <input name="example" type="text" />
+  Example value 2: <input name="example2" type="text" />
+  <input name="submit" type="submit" />
+</form>
+   <div>
+      <h1> Ans </h1>
+      <?php
+  if (isset($_POST['submit'])) {
+     $example = $_POST['example'];
+     $example2 = $_POST['example2'];
+     echo $example + $example2;
+   }
+   ?>
+   </div>
 </body>
 </html>
